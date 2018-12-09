d452a51aa229d523373a04a246f1c7b6056e65bf
i72lumav <43375310+i72lumav@users.noreply.github.com>
2018-12-09 12:41:44 +0100

    alumno.h 09-12-18

--- alumno.h
+++ alumno.h
@@ -1,48 +1,51 @@
+#include <string>
 #ifndef ALUMNO_H
 #define ALUMNO_H
-
-#include <string>
-#include <cstdlib>
-#include <iostream>
 
 using namespace std;
 
 class Alumno{
+private:
+	string DNI_;
+	string nombre_;
+	string apellidos_;
+	string direccion_;
+	string correo_;
+	string curso_;
+	string telefono_;
+	int equipo_;
+	bool lider_;
+public:
+	Alumno(string DNI, string nombre, string apellidos, string direccion, string correo, string curso, string telefono, int equipo, bool lider);	//Aqui sin comillas y punto
 
-	private:
-		string nombre_;
-		string apellidos_;
-		string dni_;
-		int telefono_;
-		string correo_;
-		string direccion_;
-		int curso_;
-		int equipo_;
-		bool lider_;
+	inline void setDNI(string DNI){DNI_=DNI;}
+	inline string getDNI(){return DNI_;}
 
-	public:
-		Alumno(string nombre="", string dni="");
-		//Observadores
-		inline string getNombre()const{return nombre_;}
-		inline string getApellidos()const{return apellidos_;}
-		inline string getDni()const{return dni_;}
-		inline int getTelefono()const{return telefono_;}
-		inline string getCorreo()const{return correo_;}
-		inline string getDireccion()const{return direccion_;}
-		inline int getCurso()const{return curso_;}
-		inline int getEquipo()const{return equipo_;}
-		inline bool isLider()const{return lider_;}
+	inline void setNombre(string nombre){nombre_=nombre;}
+	inline string getNombre(){return nombre_;}
 
-		//Constructores
-		bool setNombre(string nuevo);
-		bool setApellidos(string nuevo);
-		bool setDni(string nuevo);
-		bool setTelefono(int nuevo);
-		bool setCorreo(string nuevo);
-		bool setDireccion(string nuevo);
-		bool setCurso(int nuevo);
-		bool setEquipo(int nuevo);
-		bool setLider(bool nuevo);
-}
+	inline void setApellidos(string apellidos){apellidos_=apellidos;}
+	inline string getApellidos(){return apellidos_;}
+
+	inline void setDireccion(string direccion){direccion_=direccion;}
+	inline string getDireccion(){return direccion_;}
+
+	inline void setCorreo(string correo){correo_=correo;}
+	inline string getCorreo(){return correo_;}
+
+	inline void setCurso(string curso){curso_=curso;}
+	inline string getCurso(){return curso_;}
+
+	inline void setTelefono(string telefono){telefono_=telefono;}
+	inline string getTelefono(){return telefono_;}
+
+	inline void setEquipo(int equipo){equipo_=equipo;}
+	inline int getEquipo(){return equipo_;}
+
+	inline void setLider(bool lider){lider_=lider;}
+	inline bool getLider(){return lider_;}
+
+	inline string getApellidosyNombre(){return apellidos_ + ", " + nombre_;}
+};
 
 #endif
